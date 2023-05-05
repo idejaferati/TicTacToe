013c85592fee7b73391e9e6f61281dbabd844b71
Ideja Ferati <idejaferati03@gmail.com>
2023-05-05 22:46:19 +0200

    Updated Tic Tac Toe board layout for better visual clarity

--- game.js
+++ game.js
@@ -1,26 +1,36 @@
-let board = [  ['0','X','0'],
-    ['X','X','0'],
-    ['X','0','X']
+let board = [
+  ["X", "X", "X"],
+  ["X", "X", "0"],
+  ["X", "0", "X"],
 ];
 
-let player1 = 'X';
-let player2 = '0';
+let player1 = "X";
+let player2 = "0";
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background(220);
-    let w = width / 3;
-    let h = height / 3;
+  background(220);
+  let w = width / 3;
+  let h = height / 3;
 
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let x = w * i;
-            let y = h * j;
-            let spot = board[i][j];
-            text(spot, x, y);
-        }
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[i][j];
+      textSize(32);
+      strokeWeight(4);
+      if (spot == player2) {
+        noFill();
+        ellipse(x, y, w / 2);
+      } else if (spot == player1) {
+        let xr = w / 4;
+        line(x - xr, y - xr, x + xr, y + xr);
+        line(x + xr, y - xr, x - xr, y + xr);
+      }
     }
+  }
 }
