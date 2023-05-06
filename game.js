014d3b5680eb18ac41a69f47be87b8d5855b067c
Anisa Bicaj <anisa.bicaj@student.uni-pr.edu>
2023-05-06 20:08:54 +0200

    Added currentPlayer

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 let board = [
+<<<<<<< Updated upstream
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
@@ -34,3 +35,45 @@ function draw() {
     }
   }
 }
+=======
+    ['','',''],
+    ['','',''],
+    ['','',''],
+   ];
+   let players = ['X','0'];
+   let currentPlayer ;
+   function setup(){
+    createCanvas(400, 400);
+    if(random(1)<0.5){
+    currentPlayer=players[0];
+    }else{
+    currentPlayer=players[1];
+    }
+    
+   }
+   function draw(){
+    background(255);
+    let w = width / 3;
+    let h=height / 3;
+    
+    for (let j=0; j<3; j++){ 
+    for(let i=0; i<3; i++){
+    let x=w*i+w/2;
+    let y=h*j+h/2;
+    let spot=board[i][j];
+    textSize(32);
+    strokeWeight(4); 
+    if (spot==players[1]){
+    noFill();
+    ellipseMode(CORNER);
+    ellipse(x,y,w/2);
+    }else if (spot==players[0]){
+    let xr=w/4;
+    line(x-xr,y-zr,x+xr,y+xr);
+    line(x+xr,y-xr,x-xr,y+xr);
+    }
+    
+    }
+   }
+  }
+>>>>>>> Stashed changes
