67e8fb6ae325cbcc4bc5575b8c14db62835521bb
ArbreshaHodolli <arberesha.hodolli@student.uni-pr.edu>
2023-05-13 23:22:54 +0200

    Add alpha-beta pruning to minimax algorithm

--- minimaxAlgorithm.js
+++ minimaxAlgorithm.js
@@ -1,92 +1,98 @@
 function bestMove() {
-  let bestScore = -Infinity;
-  let move;
+    let bestScore = -Infinity;
+    let move;
 
-  // Check if it is the first move
-  let isFirstMove = true;
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (board[i][j] !== '') {
-        isFirstMove = false;
-        break;
-      }
-    }
-    if (!isFirstMove) {
-      break;
-    }
-  }
 
-  // Play in a random place if it is the first move
-  if (isFirstMove) {
-    let availableMoves = [];
+    let isFirstMove = true;
     for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == '') {
-          availableMoves.push({ i, j });
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] !== '') {
+                isFirstMove = false;
+                break;
+            }
         }
-      }
-    }
-    move = random(availableMoves);
-  } else {
-    // Otherwise, use minimax algorithm
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == '') {
-          board[i][j] = ai;
-          let score = minimax(board, 0, false);
-          board[i][j] = '';
-          if (score > bestScore) {
-            bestScore = score;
-            move = { i, j };
-          }
+        if (!isFirstMove) {
+            break;
         }
-      }
     }
-  }
 
-  board[move.i][move.j] = ai;
-  currentPlayer = person;
-}
-
-
-let scores = {
-    X:10,
-    0:-10,
-    tie:0
-}
-function minimax(board, depth,isMaximizing){
-    let result = checkWinner();
-    if(result !== null){
-        return scores[result];
-    }
-    if(isMaximizing){
-        let bestScore=-Infinity;
-        for(let i=0; i<3; i++) {
+    if (isFirstMove) {
+        let availableMoves = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    availableMoves.push({ i, j });
+                }
+            }
+        }
+        move = random(availableMoves);
+    } else {
+//  Use minimax algorithm with alpha-beta pruning
+        for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] == '') {
                     board[i][j] = ai;
-                    let score = minimax(board, depth + 1, false);
+                    let score = minimax(board, 0, false, -Infinity, Infinity);
                     board[i][j] = '';
-                    bestScore = max(score, bestScore);
-                }
-            }
-        }
-            return bestScore;
-        } else {
-            let bestScore=Infinity;
-            for(let i=0; i<3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    if (board[i][j] == '') {
-                        board[i][j] = person;
-                        let score = minimax(board, depth + 1, true);
-                        board[i][j] = '';
-                        bestScore =min(score,bestScore);
+                    if (score > bestScore) {
+                        bestScore = score;
+                        move = { i, j };
                     }
                 }
             }
-            return bestScore;
+        }
+    }
 
+    board[move.i][move.j] = ai;
+    currentPlayer = person;
+}
+
+let scores = {
+    X: 10,
+    0: -10,
+    tie: 0
+};
+
+function minimax(board, depth, isMaximizing, alpha, beta) {
+    let result = checkWinner();
+    if (result !== null) {
+        return scores[result];
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    board[i][j] = ai;
+                    let score = minimax(board, depth + 1, false, alpha, beta);
+                    board[i][j] = '';
+                    bestScore = max(score, bestScore);
+                    alpha = max(alpha, score);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    board[i][j] = person;
+                    let score = minimax(board, depth + 1, true, alpha, beta);
+                    board[i][j] = '';
+                    bestScore = min(score, bestScore);
+                    beta = min(beta, score);
+                    if (beta <= alpha) {
+                        break;
+                    }
+                }
+            }
+        }
+        return bestScore;
     }
     return 1;
 }
-
