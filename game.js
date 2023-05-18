cfb57ceaadc4e43c3110799134276ce52882c1ef
Ideja Ferati <idejaferati03@gmail.com>
2023-05-18 11:40:08 +0200

    Added CSS styles for Tic Tac Toe game

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
-let board = [  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
 let w; //= width / 3;
 let h; //= height / 3;
 
-let ai='X';
-let person='0';
+let ai = "X";
+let person = "0";
 
-let currentPlayer= person;
+let currentPlayer = person;
 
+// let isGameOver = false; // Flag variable to track game over state
 
 function setup() {
   createCanvas(400, 400);
@@ -20,8 +22,8 @@ function setup() {
   bestMove();
 }
 
-function equals3(a,b,c){
-  return (a==b && b==c && a!='');
+function equals3(a, b, c) {
+  return a == b && b == c && a != "";
 }
 
 function checkWinner() {
@@ -29,7 +31,7 @@ function checkWinner() {
 
   // horizontal
   for (let i = 0; i < 3; i++) {
-    if (equals3(board[i][0],board[i][1],board[i][2])) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
       winner = board[i][0];
       break;
     }
@@ -37,47 +39,45 @@ function checkWinner() {
 
   // vertical
   for (let i = 0; i < 3; i++) {
-    if (equals3(board[0][i],board[1][i],board[2][i])) {
+    if (equals3(board[0][i], board[1][i], board[2][i])) {
       winner = board[0][i];
       break;
     }
   }
 
   // diagonal
-  if (equals3(board[0][0],board[1][1],board[2][2])) {
+  if (equals3(board[0][0], board[1][1], board[2][2])) {
     winner = board[0][0];
   }
 
-  if (equals3(board[2][0],board[1][1],board[0][2])) {
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
     winner = board[2][0];
   }
 
-  let openSpots=0;
-  for(let i=0; i<3; i++){
-    for(let j=0; j<3; j++){
-      if(board[i][j]==''){
+  let openSpots = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == "") {
         openSpots++;
       }
     }
   }
 
   if (winner == null && openSpots == 0) {
-    return 'tie';
+    return "tie";
   } else {
     return winner;
   }
 }
 
-
-
 function mousePressed() {
-  if (document.getElementById('playWithComputer').checked) {
+  if (document.getElementById("computer")) {
     // Play with computer
     if (currentPlayer === person) {
       let i = floor(mouseX / w);
       let j = floor(mouseY / h);
 
-      if (board[i][j] === '') {
+      if (board[i][j] === "") {
         board[i][j] = currentPlayer;
         currentPlayer = ai;
         if (checkWinner() === null) {
@@ -85,12 +85,12 @@ function mousePressed() {
         }
       }
     }
-  } else {
+  } else if (document.getElementById("friends")) {
     // Play with another user
     let i = floor(mouseX / w);
     let j = floor(mouseY / h);
 
-    if (board[i][j] === '') {
+    if (board[i][j] === "") {
       board[i][j] = currentPlayer;
       if (currentPlayer === person) {
         currentPlayer = ai;
@@ -98,11 +98,8 @@ function mousePressed() {
         currentPlayer = person;
       }
     }
-
   }
 }
-
-
 
 function draw() {
   background(255);
@@ -131,15 +128,15 @@ function draw() {
     }
   }
 
-
   let result = checkWinner();
   if (result != null) {
     noLoop();
-    let resultP=createP('');
-    resultP.style('font-size','32pt');
-    if(result == 'tie'){
-      resultP.html('tie!');
-    }else {
+    let resultP = createP("");
+    resultP.class("result"); // Assign the "result" class to the element
+
+    if (result == "tie") {
+      resultP.html("tie!");
+    } else {
       resultP.html(`${result} wins!`);
     }
   }
