78b7d4262388de19420801d20ebf3eef5dfe35a9
olsajupolli <94190695+olsajupolli@users.noreply.github.com>
2023-05-19 12:30:58 +0200

    Updated multiplayer mode

--- game.js
+++ game.js
@@ -1,30 +1,59 @@
 let board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
 let w; //= width / 3;
 let h; //= height / 3;
 
-let ai = 'X';
-let person = '0';
+let playerX = "X";
+let player0 = "0";
 
-let currentPlayer = person;
-let startingPlayer = 'computer';
+let currentPlayer = player0;
+let playerLabel;
+
+let restartButton;
+let resultP;
+
+let isGameOver = false; // Flag variable to track game over state
+
+let playWithComputerCheckbox;
+let isPlayingWithComputer = true;
 
 function setup() {
   createCanvas(400, 400);
   w = width / 3;
   h = height / 3;
 
-  if (startingPlayer === 'computer') {
-    bestMove();
+  playerLabel = createP("");
+  playerLabel.class("player"); 
+  setPlayerLabel();
+
+  resultP = createP("");
+  resultP.class("result"); // Assign the "result" class to the element
+
+  bestMove();
+  playWithComputerCheckbox = document.getElementById('playWithComputer');
+  playWithComputerCheckbox.addEventListener('change', togglePlayWithComputer);
+
+  restartButton = document.getElementById('restartButton');
+  // Add a click event listener to the restart button
+  restartButton.addEventListener("click", resetGame);
+}
+
+function setPlayerLabel() {
+  if (checkWinner() !== null || isBoardFull()) {
+    playerLabel.html("Game is finished");
+  } else if (currentPlayer === player0) {
+    playerLabel.html("Player O, it's your turn");
+  } else {
+    playerLabel.html("Player X, it's your turn");
   }
 }
 
 function equals3(a, b, c) {
-  return a == b && b == c && a != '';
+  return a == b && b == c && a != "";
 }
 
 function checkWinner() {
@@ -51,57 +80,142 @@ function checkWinner() {
     winner = board[0][0];
   }
 
-  if (equals3(board[2][0],board[1][1], board[0][2])) {
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
     winner = board[2][0];
   }
 
   let openSpots = 0;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      if (board[i][j] == '') {
+      if (board[i][j] == "") {
         openSpots++;
       }
     }
   }
 
   if (winner == null && openSpots == 0) {
-    return 'tie';
+    return "tie";
   } else {
     return winner;
   }
 }
 
-function mousePressed() {
-  if (currentPlayer === person) {
-    let i = floor(mouseX / w);
-    let j = floor(mouseY / h);
+function togglePlayWithComputer() {
+  isPlayingWithComputer = playWithComputerCheckbox.checked;
+  resetGame();
+}
 
-    if (board[i][j] === '') {
-      board[i][j] = person;
-      currentPlayer = ai;
-      bestMove();
+function resetGame() {
+  // Reset the game state here
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  currentPlayer = player0;
+
+  setPlayerLabel();
+
+  isGameOver = false;
+
+  resultP.html(""); // Clear the result label
+
+  loop();
+}
+
+function handleMove(i, j) {
+  if (!isGameOver && board[i][j] === "") {
+    board[i][j] = currentPlayer;
+    if (currentPlayer === player0) {
+      currentPlayer = playerX;
+    } else {
+      currentPlayer = player0;
+    }
+    setPlayerLabel();
+
+    if (document.getElementById("playWithComputer").checked) {
+      if (!isGameOver) {
+        playerLabel.html("Computer is thinking...");
+          setTimeout(() => {
+            bestMove();
+            setPlayerLabel();
+          }, 700);
+      }
+    }
+
+    if (checkWinner() !== null || isBoardFull()) {
+      isGameOver = true;
+      setPlayerLabel();
     }
   }
 }
 
+function mousePressed() {  
+  // Check if the restart button is clicked
+  const buttonRect = restartButton.getBoundingClientRect();
+  if (
+    mouseX >= buttonRect.left &&
+    mouseX <= buttonRect.right &&
+    mouseY >= buttonRect.top &&
+    mouseY <= buttonRect.bottom
+  ) {
+    resetGame();
+    return;
+  }
 
-function startGame(start) {
-  startingPlayer = start;
-  currentPlayer = start === 'player' ? person : ai;
-  board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
-
-  if (startingPlayer === 'computer') {
-    bestMove();
+  print('mmm');
+  if (document.getElementById("playWithComputer").checked) {
+    if (currentPlayer === player0 && !isGameOver) {
+      let i = floor(mouseX / w);
+      let j = floor(mouseY / h);
+      handleMove(i, j);
+    }
+  } else {
+    let i = floor(mouseX / w);
+    let j = floor(mouseY / h);
+    handleMove(i, j);
   }
 }
 
+// function btnPressed(type) {
+//   if (type == "computer") {
+//     // Play with computer
+//     if (currentPlayer === person) {
+//       let i = floor(mouseX / w);
+//       let j = floor(mouseY / h);
+
+//       if (board[i][j] === "") {
+//         board[i][j] = currentPlayer;
+//         currentPlayer = ai;
+//         if (checkWinner() === null) {
+//           bestMove();
+//         }
+//       }
+//     }
+//   } else if (type == "friends") {
+//     // Play with another user
+//     let i = floor(mouseX / w);
+//     let j = floor(mouseY / h);
+
+//     if (board[i][j] === "") {
+//       board[i][j] = currentPlayer;
+//       if (currentPlayer === person) {
+//         currentPlayer = ai;
+//       } else {
+//         currentPlayer = person;
+//       }
+//     }
+
+//   }
+// }
+
+// function restart() {
+//   print('restart');
+// }
+
 function draw() {
   background(255);
-  strokeWeight(4);
+  strokeWeight(8);
 
   line(w, 0, w, height);
   line(w * 2, 0, w * 2, height);
@@ -114,11 +228,11 @@ function draw() {
       let y = h * j + h / 2;
       let spot = board[i][j];
       textSize(32);
-      strokeWeight(4);
-      if (spot == person) {
+      strokeWeight(15);
+      if (spot == player0) {
         noFill();
         ellipse(x, y, w / 2);
-      } else if (spot == ai) {
+      } else if (spot == playerX) {
         let xr = w / 4;
         line(x - xr, y - xr, x + xr, y + xr);
         line(x + xr, y - xr, x - xr, y + xr);
@@ -129,29 +243,22 @@ function draw() {
   let result = checkWinner();
   if (result != null) {
     noLoop();
-    let resultP = createP('');
-    resultP.style('font-size', '32pt');
-    if (result == 'tie') {
-      resultP.html('Tie!');
+
+    if (result == "tie") {
+      resultP.html("tie!");
     } else {
       resultP.html(`${result} wins!`);
-    }
-  } else {
-    // If the current player is the computer, make its move
-    if (currentPlayer === ai) {
-      bestMove();
     }
   }
 }
 
-let result = checkWinner();
-if (result != null) {
-  noLoop();
-  let resultP = createP('');
-  resultP.style('font-size', '32pt');
-  if (result == 'tie') {
-    resultP.html('Tie!');
-  } else {
-    resultP.html(`${result} wins!`);
+function isBoardFull() {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === "") {
+        return false;
+      }
+    }
   }
+  return true;
 }
--- minimaxAlgorithm.js
+++ minimaxAlgorithm.js
@@ -1,101 +1,51 @@
 function bestMove() {
-    if (currentPlayer === ai) {
-        let bestScore = -Infinity;
-        let move;
+    let bestScore = -Infinity;
+    let move;
 
-        let isFirstMove = true;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (board[i][j] !== '') {
-                    isFirstMove = false;
-                    break;
-                }
-            }
-            if (!isFirstMove) {
+
+    let isFirstMove = true;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] !== '') {
+                isFirstMove = false;
                 break;
             }
         }
+        if (!isFirstMove) {
+            break;
+        }
+    }
 
-        if (isFirstMove && startingPlayer === 'computer') {
-            // Computer makes the first move randomly
-            let availableMoves = [];
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    if (board[i][j] === '') {
-                        availableMoves.push({ i, j });
-                    }
+    if (isFirstMove) {
+        let availableMoves = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    availableMoves.push({ i, j });
                 }
             }
-            move = random(availableMoves);
-        } else {
-            // Use minimax algorithm with alpha-beta pruning
-            let alpha = -Infinity;
-            let beta = Infinity;
-
-            function minimax(board, depth, isMaximizing, alpha, beta) {
-                let result = checkWinner();
-                if (result !== null) {
-                    return scores[result];
-                }
-
-                if (isMaximizing) {
-                    let bestScore = -Infinity;
-                    for (let i = 0; i < 3; i++) {
-                        for (let j = 0; j < 3; j++) {
-                            if (board[i][j] === '') {
-                                board[i][j] = ai;
-                                let score = minimax(board, depth + 1, false, alpha, beta);
-                                board[i][j] = '';
-                                bestScore = max(score, bestScore);
-                                alpha = max(alpha, score);
-                                if (beta <= alpha) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    return bestScore;
-                } else {
-                    let bestScore = Infinity;
-                    for (let i = 0; i < 3; i++) {
-                        for (let j = 0; j < 3; j++) {
-                            if (board[i][j] === '') {
-                                board[i][j] = person;
-                                let score = minimax(board, depth + 1, true, alpha, beta);
-                                board[i][j] = '';
-                                bestScore = min(score, bestScore);
-                                beta = min(beta, score);
-                                if (beta <= alpha) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    return bestScore;
-                }
-            }
-
-            // Find the best move using minimax algorithm
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    if (board[i][j] === '') {
-                        board[i][j] = ai;
-                        let score = minimax(board, 0, false, alpha, beta);
-                        board[i][j] = '';
-                        if (score > bestScore) {
-                            bestScore = score;
-                            move = { i, j };
-                        }
+        }
+        move = random(availableMoves);
+    } else {
+//  Use minimax algorithm with alpha-beta pruning
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    board[i][j] = playerX;
+                    let score = minimax(board, 0, false, -Infinity, Infinity);
+                    board[i][j] = '';
+                    if (score > bestScore) {
+                        bestScore = score;
+                        move = { i, j };
                     }
                 }
             }
         }
-
-        board[move.i][move.j] = ai;
-        currentPlayer = person;
     }
-}
 
+    board[move.i][move.j] = playerX;
+    currentPlayer = player0;
+}
 
 let scores = {
     X: 10,
@@ -114,7 +64,7 @@ function minimax(board, depth, isMaximizing, alpha, beta) {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] == '') {
-                    board[i][j] = ai;
+                    board[i][j] = playerX;
                     let score = minimax(board, depth + 1, false, alpha, beta);
                     board[i][j] = '';
                     bestScore = max(score, bestScore);
@@ -131,7 +81,7 @@ function minimax(board, depth, isMaximizing, alpha, beta) {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] == '') {
-                    board[i][j] = person;
+                    board[i][j] = player0;
                     let score = minimax(board, depth + 1, true, alpha, beta);
                     board[i][j] = '';
                     bestScore = min(score, bestScore);
